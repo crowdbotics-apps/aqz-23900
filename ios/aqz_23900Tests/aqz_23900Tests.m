e84a1963b667738e614895e5ae3fb6c2dec53f6d
Crowdbotics <team@crowdbotics.com>
2021-01-17 17:38:37 +0000

    Initial commit.

---
+++ ios/aqz_23900Tests/aqz_23900Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface aqz_23900 : XCTestCase
+
+@end
+
+@implementation aqz_23900
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
